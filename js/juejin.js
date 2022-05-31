be23554df8d3e72ca6f5ab8ca4475dd0af8f2273
yuyue <yy6146820566@163.com>
2022-05-31 10:35:53 +0800

    视频模式

--- js/juejin.js
+++ js/juejin.js
@@ -6,7 +6,8 @@ chrome.runtime.onMessage.addListener(
       'init':skinInit,
       'dark':skinDark,
       'video-fengchuimailang':skinVideoFcml,
-      'video-tonghuazhen':skinVideoFcmlThz,
+      'video-huanghun':skinVideoHh,
+      'video-yuai':skinVideoYa,
     }
     if (request.action == "setJueJinCss") {
       console.log(request);
@@ -27,16 +28,37 @@ function skinDark(){
 }
 // 视频模式-风吹麦浪
 function skinVideoFcml(){
-  const flag = $('.my-video').length <1
-  if(flag){
-    const node = `<video src='https://img.tukuppt.com/video_show/2419216/00/01/75/5b4c1aeda31ca.mp4' 
-    autoplay loop class='my-video'></video>`
-    $('body').append(node);
-  }
+  const vUrl = 'https://cdn.videvo.net/videvo_files/video/free/2021-08/large_watermarked/210722_05_Festival_4k_073_preview.mp4'
+  const aUrl = 'https://www.qqmc.com/mp3/music239653.mp3'
+  videoMooe(vUrl, aUrl)
 }
-// 视频模式-童话镇
-function skinVideoFcmlThz(){
+// 视频模式-黄昏
+function skinVideoHh(){
+  // const vUrl = 'https://cdn.videvo.net/videvo_files/video/free/2018-01/large_watermarked/171124_E1_HD_012_preview.mp4'
+  const vUrl = 'https://cdn.videvo.net/videvo_files/video/free/2015-09/large_watermarked/sunrise_over_the_lake2_preview.mp4'
+  const aUrl = 'https://www.qqmc.com/mp3/music6874743.mp3'
+  videoMooe(vUrl, aUrl)
+}
+// 视频模式-雨爱
+function skinVideoYa(){
+  const vUrl = 'https://cdn.videvo.net/videvo_files/video/free/2020-01/large_watermarked/200116_Lens%20effect_4k_073_preview.mp4'
+  const aUrl = 'https://www.qqmc.com/mp3/music38615712.mp3'
+  videoMooe(vUrl, aUrl)
+}
 
+// 视频模式前置动作
+function videoMooe(vUrl='', aUrl=''){
+  const flag = $('.my-video').length <1
+  const node = `<video id="bg_video" src='${vUrl}' autoplay loop class='my-video'></video>`
+  const audioNode = `<audio id="bg_music" class='my-audio' src='${aUrl}' controls loop autoplay></audio>`
+  if(flag){
+    $('body').append(node)
+    $('body').append(audioNode)
+    $('body').addClass('jj-video-box')
+  }else{
+    $('#bg_video').attr('src',vUrl)
+    $('#bg_music').attr('src',aUrl)
+  }
 }
 
 $(function(){
